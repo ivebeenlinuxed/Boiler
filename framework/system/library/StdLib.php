d5255f95388cd5780cd42a418ab61a9efae367b0
Will Tinsdeall <ivebeenlinuxed@googlemail.com>
2012-02-04 20:35:30 +0000

    Added an entity_decode function to StdLib from php.net user comments

--- framework/system/library/StdLib.php
+++ framework/system/library/StdLib.php
@@ -2,6 +2,14 @@
 namespace System\Library;
 
 class StdLib {
+	public static function decode_entities($text) {
+	    $text= html_entity_decode($text,ENT_QUOTES,"ISO-8859-1"); #NOTE: UTF-8 does not work!
+	    $text= preg_replace('/&#(\d+);/me',"chr(\\1)",$text); #decimal notation
+	    $text= preg_replace('/&#x([a-f0-9]+);/mei',"chr(0x\\1)",$text);  #hex notation
+	    return $text;
+	}
+	
+	
 	public static function has_post($param) {
 		foreach ($param as $data) {
 			if (!isset($_POST[$data])) {
