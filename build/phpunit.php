ec10598f2a4341130a79370bdda3762166f1bf92
Will Tinsdeall <ivebeenlinuxed@googlemail.com>
2012-05-20 00:04:49 +0100

    Changes to builders

--- build/phpunit.php
+++ build/phpunit.php
@@ -1,6 +1,4 @@
 <?php
 $_SERVER['no_run'] = true;
 require __DIR__."/../htdocs/index.php";
-//require __DIR__."/phpwebdriver/WebDriver.php";
 require_once "Selenium2PHPUnit/bootstrap.php";
-require __DIR__."/../tests/BaseTestcase.php";
