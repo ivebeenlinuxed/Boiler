802a894c5248026ae87eef61e26c583ff1d0db00
Will Tinsdeall <ivebeenlinuxed@googlemail.com>
2012-05-20 16:15:32 +0100

    Changes to framework URLs

--- framework/system/controller/FrameworkLicence.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Controller;
-
-class FrameworkLicence {
-	function index() {
-		echo "TEST";
-	}
-}
--- framework/system/controller/Frameworklicence.php
+++ framework/system/controller/Frameworklicence.php
@@ -1,14 +1,14 @@
 <?php
 namespace Controller;
 
-class Frameworklicence {
+class Framework {
 	function index() {
 		echo "TEST";
 	}
 	
 	function phpunit() {
 		if (\Core\Router::$settings['test']['enabled'] == 1) {
-			include BOILER_LOCATION."../build/selenium/phpunit_coverage.php";
+			include BOILER_LOCATION."../build/Selenium2PHPUnit/phpunit_coverage.php";
 		}
 	}
 }
