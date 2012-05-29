e4e7bd27b80ec76914652d24c718cd2625eeddb0
Will Tinsdeall <ivebeenlinuxed@googlemail.com>
2012-05-29 15:22:03 +0100

    Removed Selenium2PHPUnit framework - this should not be bundled

--- build/Selenium2PHPUnit/WebDriver/MouseButton.php
+++
@@ -1,6 +0,0 @@
-<?php
-class MouseButton {
-	const LEFT = 0;
-	const MIDDLE = 1;
-	const RIGHT = 2;
-}
--- build/Selenium2PHPUnit/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__."/WebDriver/WebDriver.php";
-require __DIR__."/Selenium2TestSuite.php";
-require __DIR__."/Selenium2TestCase.php";
-require __DIR__."/Selenium2TestDriver.php";
-require __DIR__."/Selenium2BrowserTestSuite.php";
