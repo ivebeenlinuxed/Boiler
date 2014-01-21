c983448ad736b2ee44f239afc4e4efded22e4b9d
Will Tinsdeall <will@bluelightstudios.co.uk>
2014-01-21 23:24:38 +0000

    Dependencies and a ready-made template

---
+++ framework/application/view/template/bottom.php
@@ -0,0 +1,17 @@
+<div class="container">
+		<footer>
+			<hr>
+			<p>&copy; William Tinsdeall 2014</p>
+		</footer>
+</div>
+<script type="text/javascript" src="/plugins/boiler/utils.js"></script>
+<script type="text/javascript" src="/js/main_ajax.js"></script>
+<script type="text/javascript" src="/plugins/jquery-pjax/jquery.pjax.js"></script>
+<script type="text/javascript" src="/js/main_ajax.js"></script>
+
+
+<script type="text/javascript" src="/plugins/boiler/widget/search_expression/search_expression.js"></script>
+<script type="text/javascript" src="/js/tooltip-loader.js"></script>
+<script type="text/javascript" src="/plugins/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ framework/application/view/template/top.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<title>Lichfield Online</title>
+<link rel="stylesheet" type="text/css" href="/plugins/bootstrap/css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="/css/main.css" />
+<link rel="stylesheet" type="text/css" href="/css/slides.css" />
+<link rel="stylesheet" type="text/css" href="/css/group/profile.css" />
+<link rel="stylesheet" type="text/css" href="/plugins/font-awesome-4.0.3/css/font-awesome.min.css" />
+<script type="text/javascript" src="/plugins/jquery/jquery-1.9.1.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="/plugins/boiler/widget/search_expression/search_expression.css" />
+</head>
+<body>
+	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse"
+				data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span> <span
+					class="icon-bar"></span> <span class="icon-bar"></span> <span
+					class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="/">Lichfield Online</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse"
+			id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="/group">Do</a></li>
+				<li><a href="/facility">Eat</a></li>
+				<li><a href="/facility">See</a></li>
+			</ul>
+			<form class="navbar-form navbar-left" role="search">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Search">
+				</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">About Us</a></li>
+				<li><a href="/contact">Login</a></li>
+			</ul>
+		</div>
+		<!-- /.navbar-collapse -->
+		</div>
+	</nav>
