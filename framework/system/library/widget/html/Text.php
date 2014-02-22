30745add7ceba361f4a170e6cd23ad61896eafd0
Will Tinsdeall <will@bluelightstudios.co.uk>
2014-02-22 14:11:32 +0000

    Fixed text widget

--- framework/system/library/widget/html/Text.php
+++ framework/system/library/widget/html/Text.php
@@ -2,4 +2,16 @@
 namespace System\Library\Widget\Html;
 
 class Text extends \Library\Widget\Widget {
+	public $result = "";
+	public function Render() {
+		if ($this->edit_mode) {
+			echo "<input type='text' class='form-control' value=\"".htmlentities($this->result)."\" {$this->getDataFields()} />";
+		} else {
+			echo "<span data-table='{$this->table}' data-field='{$this->field}' data-id='{$this->id}'>".htmlentities($this->result)."</span>";
+		}
+		}
+	
+		public function setResult($result) {
+		$this->result = $result;
+	}
 }
